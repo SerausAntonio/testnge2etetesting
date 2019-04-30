9897c901e9e1d77e2a76448c9b446b20695f6628
Antonio Seraus <antonioseraus@onior.com>
2019-04-30 14:16:11 +0200

    testng e2e second commit

--- src/test/java/parameterization/TestNGParameterizationExcel.java
+++ src/test/java/parameterization/TestNGParameterizationExcel.java
@@ -4,16 +4,32 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestNGParameterizationExcel {
+	public static XLS_Reader excel = null;
 
 	@Test(dataProvider="getData")
 	public void testData(String username, String password, String is_correct) {
-		
+		System.out.println(username + "--" + password + "---" + is_correct);
 		
 	}
 	
 	@DataProvider
 	public static Object[][] getData(){
-		Object[][] data = new Object[2][3];
+		
+		if (excel == null) {
+			excel = new XLS_Reader("C:\\Users\\Antonio\\Desktop\\testdata.xlsx");
+		}
+		String sheetName= "loginTest";
+		int rows = excel.getRowCount(sheetName);
+		int cols = excel.getColumnCount(sheetName);
+		
+		Object[][] data = new Object[rows-1][cols];
+		
+		for (int rowNum=2; rowNum <= rows; rowNum++) {
+			
+			for (int colNum=0; colNum < cols; colNum++) {
+				data[rowNum-2][colNum] = excel.getCellData(sheetName, colNum, rowNum);
+			}
+		}
 		
 		return data;
 	}
